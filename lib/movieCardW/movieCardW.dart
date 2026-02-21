1ec5de1202cbc43ba2678619cdae58f9b542da12
ValGorovyi <valvetigu@gmail.com>
2026-02-21 18:26:45 +0200

    continue with movieDetailsW

--- lib/movieCardW/movieCardW.dart
+++ lib/movieCardW/movieCardW.dart
@@ -336,6 +336,13 @@ class _ColumnEmployeeW extends StatelessWidget {
 class ActorScrolingW extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final model = UniversalInheritNitifier.watch<MovieCardDetailsModel>(
+      context,
+    );
+    var casts = model?.movieDetails?.credits.cast;
+
+    if (casts == null || casts.isEmpty) return SizedBox.fromSize();
+    final lengthBuilder = casts.length;
     return ColoredBox(
       color: Colors.white70,
       child: Column(
@@ -346,54 +353,9 @@ class ActorScrolingW extends StatelessWidget {
             child: Text('Series Cart'),
           ),
           SizedBox(
-            height: 300,
+            height: 242,
             child: Scrollbar(
-              child: ListView.builder(
-                itemCount: 20,
-                itemExtent: 120,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, index) {
-                  return Padding(
-                    padding: EdgeInsetsGeometry.all(8),
-                    child: DecoratedBox(
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.shade300,
-
-                            blurRadius: 8,
-                            offset: Offset(0, 2),
-                          ),
-                        ],
-                        border: Border.all(color: Colors.black12),
-                        borderRadius: BorderRadiusGeometry.circular(10),
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(8)),
-                        clipBehavior: Clip.hardEdge,
-                        child: Column(
-                          children: [
-                            Image(image: AssetImage(ImagesWidg.actor)),
-                            Padding(
-                              padding: EdgeInsets.all(8),
-                              child: Column(
-                                children: [
-                                  Text('Manoj Kumar', maxLines: 1),
-                                  Text(
-                                    'Kumar was honoured with the Padma Shri in 1992 and Dadasaheb Phalke Award in 2015 by the Government of India for his contribution to Indian cinema and arts.',
-                                    maxLines: 4,
-                                  ),
-                                  Text('Episots: 4', maxLines: 1),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
+              child: _ActorListBuilder(lengthBuilder: lengthBuilder),
             ),
           ),
           Padding(
@@ -404,6 +366,77 @@ class ActorScrolingW extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _ActorListBuilder extends StatelessWidget {
+  final int lengthBuilder;
+  const _ActorListBuilder({super.key, required this.lengthBuilder});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: lengthBuilder,
+      itemExtent: 120,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (BuildContext context, int index) {
+        return _ActorItemW(actorIndex: index);
+      },
+    );
+  }
+}
+
+class _ActorItemW extends StatelessWidget {
+  final int actorIndex;
+  const _ActorItemW({super.key, required this.actorIndex});
+
+  @override
+  Widget build(BuildContext context) {
+    final model = UniversalInheritNitifier.read<MovieCardDetailsModel>(context);
+    final actorItemData = model!.movieDetails!.credits.cast[actorIndex];
+    final actorImagePath = actorItemData.profilePath;
+    return Padding(
+      padding: EdgeInsetsGeometry.all(8),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade300,
+
+              blurRadius: 8,
+              offset: Offset(0, 2),
+            ),
+          ],
+          border: Border.all(color: Colors.black12),
+          borderRadius: BorderRadiusGeometry.circular(10),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          clipBehavior: Clip.hardEdge,
+          child: Column(
+            children: [
+              actorImagePath != null
+                  ? Image.network(
+                      ApiClient.imageUrl(actorImagePath),
+                      width: 120,
+                      height: 120,
+                      fit: BoxFit.fitWidth,
+                    )
+                  : SizedBox.shrink(),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    Text(actorItemData.name, maxLines: 1),
+                    Text(actorItemData.character ?? '!!!', maxLines: 3),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
